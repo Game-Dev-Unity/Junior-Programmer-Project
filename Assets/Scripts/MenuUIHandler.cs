ef40e24ae9c107ae1c28587e72553ff21eadc92e
Numaan Javed <naumaan.javed@hotmail.com>
2025-12-22 00:43:57 +0500

    DataPersistence Done

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -9,16 +14,33 @@ using UnityEngine;
 public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
+    private MainManager mainManager;
 
     public void NewColorSelected(Color color)
     {
+        mainManager.TeamColor = color;
         // add code here to handle when a color is selected
     }
     
     private void Start()
     {
+        mainManager = GameObject.Find("MainManager").GetComponent<MainManager>();
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(mainManager.TeamColor);
+    }
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        mainManager.SaveColor();
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
     }
 }
